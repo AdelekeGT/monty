d133d435cea000cdcc146e9f66734af962d1f881
SirJupiter <jupitertoby@gmail.com>
2024-01-18 17:24:12 +0100

    working

--- do_push.c
+++ do_push.c
@@ -18,9 +18,14 @@ void do_push(stack_t **n_head, unsigned int line_number)
 			if (!isdigit(monty_object.argu[_k]))
 				not_int = 1;
 		}
+		if (not_int == 1)
+		{
+			do_print_error(0, line_number);
+			all_free(monty_object.m_file, monty_object.file_content, *n_head);
+			exit(EXIT_FAILURE);
+		}
 	}
-
-	if (monty_object.argu == NULL || not_int == 1)
+	else
 	{
 		do_print_error(0, line_number);
 		all_free(monty_object.m_file, monty_object.file_content, *n_head);
